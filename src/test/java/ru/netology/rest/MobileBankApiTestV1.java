a92db7d55be8d0c23c337e0ad72751bd2e2a1701
*V-adim13* <pogopogo21696@gmail.com>
2022-12-16 17:19:33 +0500

    shema json and test currency RUB

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,21 +1,60 @@
 package ru.netology.rest;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
+import java.util.regex.Matcher;
+
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
 
 class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200);
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void shouldJsonSchema() {
+        // код теста
+        given()
+                .then()
+                .statusCode(200)
+                // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+
+
+    }
+
+    @Test
+    void ShouldRemoteAddress() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .header("Content-Type", "application/json; charset=UTF-8");
+
+    }
+
+    @Test
+    void shouldCurrencyRUB() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .body("[2].currency", equalTo("RUB"));
+
     }
 }
